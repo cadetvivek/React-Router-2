900984f6448a409e001145e8fea700b39e9cc5c4
cadetvivek <kushwahvivek805@gmail.com>
2024-01-16 13:44:22 +0530

    find error

---
+++ my-react-router-app/src/pages/About.jsx
@@ -0,0 +1,13 @@
+// src/pages/About.jsx
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h2>About Page</h2>
+      <p>Learn more about us here.</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ my-react-router-app/src/pages/Contact.jsx
@@ -0,0 +1,13 @@
+// src/pages/Contact.jsx
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div>
+      <h2>Contact Page</h2>
+      <p>Feel free to reach out to us!</p>
+    </div>
+  );
+};
+
+export default Contact;
